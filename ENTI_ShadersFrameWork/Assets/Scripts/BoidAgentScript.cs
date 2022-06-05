cdccc32963f5b282ebd20a917ce8c95ba4be2a37
Joel <joel.mateu@enti.cat>
2022-06-05 02:20:48 +0200

    Flocking done

---
+++ ENTI_ShadersFrameWork/Assets/Scripts/BoidAgentScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidAgentScript : MonoBehaviour
+{
+    [SerializeField]
+    float
+        minSpeed = 5,
+        maxSpeed = 8,
+        perceptionRadius = 2.5f,
+        avoidanceRadius = 1,
+        maxSteerForce = 8,
+        alignWeight = 2,
+        cohesionWeight = 1,
+        separateWeight = 2.5f,
+        targetWeight = 2,
+        boundsRadius = 0.27f,
+        avoidCollisionWeight = 10,
+        collisionAvoidDst = 5;
+    [SerializeField] LayerMask obstacleMask;
+
+    public Vector3 Position { get { return transform.position; } }
+    public Vector3 Forward { get { return transform.forward; } }
+    Vector3 vel;
+
+    [HideInInspector]
+    public Vector3 avgFlockHeading;
+    [HideInInspector]
+    public Vector3 avgAvoidanceHeading;
+    [HideInInspector]
+    public Vector3 centreOfFlockmates;
+    [HideInInspector]
+    public int numFlockmates;
+
+    Transform target;
+
+    public void Init(Transform _target)
+    {
+        target = _target;
+        vel = transform.forward * minSpeed;
+    }
+
+    private void Update()
+    {
+        Vector3 acc = SteerTowards(target.position - Position) * targetWeight;
+        if (numFlockmates > 0)
+        {
+            acc += SteerTowards(avgFlockHeading) * alignWeight; // Aligment Force
+            centreOfFlockmates /= numFlockmates;
+            acc += SteerTowards(centreOfFlockmates - Position) * cohesionWeight; // Cohesion Force
+            acc += SteerTowards(avgAvoidanceHeading) * separateWeight;  // Separation Force
+        }
+
+        if (IsHeadingForCollision())
+        {
+            Vector3 collisionAvoidDir = ObstacleRays();
+            Vector3 collisionAvoidForce = SteerTowards(collisionAvoidDir) * avoidCollisionWeight;
+            acc += collisionAvoidForce;
+        }
+
+        vel += acc * Time.deltaTime;
+        float speed = vel.magnitude;
+        Vector3 dir = vel / speed;
+        speed = Mathf.Clamp(speed, minSpeed, maxSpeed);
+        vel = dir * speed;
+
+        transform.position += vel * Time.deltaTime;
+        transform.forward = dir;
+    }
+
+    bool IsHeadingForCollision()
+    {
+        RaycastHit hit;
+        if (Physics.SphereCast(Position, boundsRadius, Forward, out hit, collisionAvoidDst, obstacleMask))
+            return true;
+        else
+            return false;
+    }
+
+    Vector3 ObstacleRays()
+    {
+        Vector3[] raysDirections = BoidManagerScript.RaysDirections;
+
+        for (int i = 0; i < raysDirections.Length; i++)
+        {
+            Vector3 dir = target.TransformDirection(raysDirections[i]);
+            Ray ray = new Ray(Position, dir);
+            if (!Physics.SphereCast(ray, boundsRadius, collisionAvoidDst, obstacleMask))
+            {
+                return dir;
+            }
+        }
+
+        return Forward;
+    }
+
+    Vector3 SteerTowards(Vector3 vector)
+    {
+        Vector3 v = vector.normalized * maxSpeed - vel;
+        return Vector3.ClampMagnitude(v, maxSteerForce);
+    }
+
+}
---
+++ ENTI_ShadersFrameWork/Assets/Scripts/BoidManagerScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManagerScript : MonoBehaviour
+{
+    public Transform target;
+    public ComputeShader boidCmptShader;
+    public float awarenessDist = 2.5f, avoidanceDist = 1;
+    public int viewDirectionsAmount = 300;
+
+    BoidAgentScript[] boids;
+    static Vector3[] raysDirections;
+    public static Vector3[] RaysDirections { get { return raysDirections; } }
+
+    void Start()
+    {
+        boids = FindObjectsOfType<BoidAgentScript>();
+        foreach (BoidAgentScript boidAgent in boids)
+            boidAgent.Init(target);
+
+        CalculateRaysDirections(viewDirectionsAmount);
+    }
+
+    void Update()
+    {
+        if (boids == null) return;
+
+        BoidInfo[] boidsData = new BoidInfo[boids.Length];
+        for (int i = 0; i < boids.Length; i++)
+        {
+            boidsData[i].pos = boids[i].Position;
+            boidsData[i].dir = boids[i].Forward;
+        }
+
+        var boidsBuffer = new ComputeBuffer(boids.Length, BoidInfo.Size);
+        boidsBuffer.SetData(boidsData);
+
+        int kernelHandle = boidCmptShader.FindKernel("CSMain");
+        boidCmptShader.SetBuffer(kernelHandle, "_boids", boidsBuffer);
+        boidCmptShader.SetInt("_boidsLength", boids.Length);
+        boidCmptShader.SetFloat("_awarenessDist", awarenessDist);
+        boidCmptShader.SetFloat("_avoidanceDist", avoidanceDist);
+
+        int threadGroups = Mathf.CeilToInt(boids.Length / 1024.0f);
+        boidCmptShader.Dispatch(kernelHandle, threadGroups, 1, 1);
+
+        boidsBuffer.GetData(boidsData);
+
+        for (int i = 0; i < boids.Length; i++)
+        {
+            boids[i].avgFlockHeading = boidsData[i].flockHeading;
+            boids[i].centreOfFlockmates = boidsData[i].flockCentre;
+            boids[i].avgAvoidanceHeading = boidsData[i].avoidanceHeading;
+            boids[i].numFlockmates = boidsData[i].numFlockmates;
+        }
+
+        boidsBuffer.Release();
+    }
+
+    void CalculateRaysDirections(int _viewDirectionsAmount)
+    {
+        raysDirections = new Vector3[_viewDirectionsAmount];
+
+        float goldenRatio = (1 + Mathf.Sqrt(5)) / 2;
+        float angleIncrement = Mathf.PI * 2 * goldenRatio;
+
+        for (int i = 0; i < _viewDirectionsAmount; i++)
+        {
+            float t = (float)i / _viewDirectionsAmount;
+            float inclination = Mathf.Acos(1 - 2 * t);
+            float azimuth = angleIncrement * i;
+
+            float x = Mathf.Sin(inclination) * Mathf.Cos(azimuth);
+            float y = Mathf.Sin(inclination) * Mathf.Sin(azimuth);
+            float z = Mathf.Cos(inclination);
+            raysDirections[i] = new Vector3(x, y, z);
+        }
+
+    }
+
+
+    public struct BoidInfo
+    {
+        public Vector3 pos;
+        public Vector3 dir;
+
+        public Vector3 flockHeading;
+        public Vector3 flockCentre;
+        public Vector3 avoidanceHeading;
+        public int numFlockmates;
+
+        public static int Size
+        {
+            get
+            {
+                return sizeof(float) * 3 * 5 + sizeof(int);
+            }
+        }
+    }
+}
